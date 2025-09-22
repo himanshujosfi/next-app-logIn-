447660fc72caa1de296e2036e1b94fd40d877405
joshi <hemjoshi395@gmail.com>
2025-09-22 11:26:31 +0530

    middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const authToken = request.cookies.get("auth_session")?.value
+    console.log("toekn ", authToken)
+    const path = request.nextUrl.pathname
+    console.log("path", path)
+    const authPath = path === "/logIn" || path === "/register"
+    //  Case 1: User has token
+    if (authToken) {
+        if (authPath) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+        return NextResponse.next();
+    }
+
+    //  Case 2: No token
+    if (!authToken) {
+        if (!authPath) {
+            return NextResponse.redirect(new URL("/login", request.url));
+        }
+        return NextResponse.next();
+    }
+}
+
+export const config = {
+    matcher: ['/', '/logIn', '/register'],
+}
